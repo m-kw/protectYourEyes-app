3062bf4105c9a3d669e1d9f331bcce241e55a80a
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-07 11:55:15 +0100

    Show description when app's status is off

--- app/script.js
+++ app/script.js
@@ -3,17 +3,22 @@ import { render } from 'react-dom';
 
 class App extends React.Component {
   state = {
-    status: off,
+    status: 'off',
     time: '00:20',
     timer: null,
   }
 
   render() {
+    const { status, time, timer } = this.state;
+
     return (
       <div>
         <h1>Protect your eyes</h1>
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
+        {status === 'off' &&
+        <div className="description">
+          <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+          <p>This app will help you track your time and inform you when it's time to rest.</p>
+        </div>}
         <img src="./images/work.png" />
         <img src="./images/rest.png" />
         <div className="timer">
